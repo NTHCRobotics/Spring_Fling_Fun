e6e0682f58a906bb06375b0abce7538dafe73eb1
nbala123 <111918527+nbala123@users.noreply.github.com>
2025-08-12 11:21:31 -0500

    naming fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pew_Pew_Boom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pew_Pew_Boom.java
@@ -11,23 +11,23 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Pew_Pew_Sample_Goes_Boom", group = "Sample_Goes_Boom")
 public class Pew_Pew_Boom extends OpMode {
-private DcMotorEx missleLaucher;
-private CRServo liftBoom;
+private DcMotorEx wheel;
+private CRServo liftServo1;
 private DcMotorEx sampleExplosion;
 private DcMotorEx sampleExplosion2;
-private Servo holdBoom;
-private CRServo armBoom;
+private Servo pusher;
+private CRServo liftServo2;
 
 private final ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void init(){
-        missleLaucher = hardwareMap.get(DcMotorEx.class, "missleLaucher");
-        liftBoom = hardwareMap.get(CRServo.class, "liftBoom");
+        wheel = hardwareMap.get(DcMotorEx.class, "white");
+        liftServo1 = hardwareMap.get(CRServo.class, "liftservo1");
         sampleExplosion = hardwareMap.get(DcMotorEx.class, "sampleExplosion");
         sampleExplosion2 = hardwareMap.get(DcMotorEx.class, "sampleExplosion2");
-        holdBoom = hardwareMap.get(Servo.class, "holdBoom");
-        armBoom = hardwareMap.get(CRServo.class, "armBoom");
+        pusher = hardwareMap.get(Servo.class, "pusher");
+        liftServo2 = hardwareMap.get(CRServo.class, "liftservo2");
 
         sampleExplosion.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         sampleExplosion2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -66,12 +66,12 @@ private final ElapsedTime runtime = new ElapsedTime();
 
     public void HoldKaboom(){
         if (gamepad1.right_bumper) {
-            holdBoom.setPosition(1);
+            pusher.setPosition(1);
             runtime.reset();
 
         }
         if (runtime.seconds() > 0.3){
-            holdBoom.setPosition(0);
+            pusher.setPosition(0);
         }
     }
 
@@ -79,20 +79,20 @@ private final ElapsedTime runtime = new ElapsedTime();
         double x = -gamepad1.left_stick_x;
         double speedmod = 1.5;
 
-        missleLaucher.setPower(x*speedmod);
+        wheel.setPower(x*speedmod);
     }
 
     public void MissleAim(){
         if (gamepad1.dpad_up){
-            liftBoom.setPower(0.01);
-            armBoom.setPower(0.01);
+            liftServo1.setPower(0.01);
+            liftServo2.setPower(0.01);
         } else if (gamepad1.dpad_down) {
-            liftBoom.setPower(-0.01);
-            armBoom.setPower(-0.01);
+            liftServo1.setPower(-0.01);
+            liftServo2.setPower(-0.01);
         }
         else if (gamepad1.dpad_left){
-            liftBoom.setPower(0);
-       armBoom.setPower(0);
+            liftServo1.setPower(0);
+       liftServo2.setPower(0);
         }
 
 
